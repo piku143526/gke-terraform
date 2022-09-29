aec9195e14692e73851382916220771c9c17e0db
Pradeep <104751531+piku143526@users.noreply.github.com>
2022-09-29 17:22:17 +0530

    Update node-pools.tf

--- node-pools.tf
+++ node-pools.tf
@@ -6,7 +6,7 @@ resource "google_service_account" "kubernetes" {
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
 resource "google_container_node_pool" "general" {
   name       = "general"
-  cluster    = google_container_cluster.primary.id
+  cluster    = google_container_cluster.primary1.id
   node_count = 1
 
   management {
